b046b8d3e3445b4cb5f8f9d06779e68a718a8c81
Billy Wood <billyc.wood4@gmail.com>
2025-02-11 00:35:16 -0700

    try catch block for profile page

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -4,26 +4,31 @@ import ProfilePicture from '../../ui/dashboard/profile/ProfilePicture';
 
 
 export default async function Profile() {
+    try{
 
-  const session = await getSession();
-  const id = session?.user.name;
-  const author = await getAuthor({id});
-
-  return ( 
-    <div className='pt-16 pl-16'>
-      <div className='flex items-center'>
-        <ProfilePicture picture={author.picture} name={author.name} />
-        <div className='ml-2'>
-          <p className='text-xl'>{author.name}</p>
-          <p className='text-xs'>Author ID: {author.id}</p>
-        </div>
-      </div>
-      
-      <div>
-        <p className='mt-10'>Biography:</p>
-        <p className='mt-2 border-[1px] p-2 border-black w-4/5'>{author.bio}</p>
-      </div>
-    </div>
-  );
+        const session = await getSession();
+        const id = session?.user.name;
+        const author = await getAuthor({id});
+        
+        return ( 
+            <div className='pt-16 pl-16'>
+                <div className='flex items-center'>
+                    <ProfilePicture picture={author.picture} name={author.name} />
+                    <div className='ml-2'>
+                        <p className='text-xl'>{author.name}</p>
+                        <p className='text-xs'>Author ID: {author.id}</p>
+                    </div>
+                </div>
+                    
+                <div>
+                    <p className='mt-10'>Biography:</p>
+                    <p className='mt-2 border-[1px] p-2 border-black w-4/5'>{author.bio}</p>
+                </div>
+            </div>
+        );
+    }
+    catch(e){
+        console.log(e);
+    }
 }
 
