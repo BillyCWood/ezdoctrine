fd1af8ce6b7c5f773119dee95afcb27e5521cd53
Billy Wood <billyc.wood4@gmail.com>
2025-02-11 00:07:15 -0700

    mobile footer login button

--- app/ui/footer/ResponsiveFooter.jsx
+++ app/ui/footer/ResponsiveFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ResponsiveFooter = () => {
   return (
-    <div className='w-full lg:hidden bg-[#004aad] text-white font-light'>ResponsiveFooter</div>
+    <div className='w-full lg:hidden bg-[#004aad] text-white font-light'><a href='/api/auth/login'>Authors Only</a></div>
   )
 }
 
